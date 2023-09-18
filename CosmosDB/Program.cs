0b3c8fb4fc60eb588847c3bc76e50ee9664fb0cd
Mircea Pavileanu <mircea.pavileanu@solera.com>
2023-09-18 22:54:35 +0300

    Add project files.

---
+++ CosmosDB/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using Microsoft.Azure.Cosmos;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+public class Program
+{
+
+    private static readonly string EndpointUri = "https://pavi-cosmosdb.documents.azure.com:443/";
+
+    private static readonly string PrimaryKey = "key";
+
+    private CosmosClient cosmosClient;
+
+    private Database database;
+
+    private Container container;
+
+    private string databaseId = "az204Database";
+    private string containerId = "az204Container";
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Beginning operations...\n");
+            Program p = new Program();
+            await p.CosmosAsync();
+
+        }
+        catch (CosmosException de)
+        {
+            Exception baseException = de.GetBaseException();
+            Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e);
+        }
+        finally
+        {
+            Console.WriteLine("End of program, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public async Task CosmosAsync()
+    {
+        // Create a new instance of the Cosmos Client
+        this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+
+        // Runs the CreateDatabaseAsync method
+        await this.CreateDatabaseAsync();
+
+        // Run the CreateContainerAsync method
+        await this.CreateContainerAsync();
+    }
+    private async Task CreateDatabaseAsync()
+    {
+        this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+        Console.WriteLine("Created Database: {0}\n", this.database.Id);
+    }
+
+    private async Task CreateContainerAsync()
+    {
+        this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+        Console.WriteLine("Created Container: {0}\n", this.container.Id);
+    }
+
+}
